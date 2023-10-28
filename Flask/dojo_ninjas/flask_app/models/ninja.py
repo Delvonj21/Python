d62acfbdbcdc31186289b26a8c530b69fe18dedc
Delvon Johnson <delvonj21@gmail.com>
2023-10-28 10:15:51 +0200

    added files

---
+++ Flask/dojo_ninjas/flask_app/models/ninja.py
@@ -0,0 +1,18 @@
+from flask_app.config.mysqlconnection import connectToMySQL
+
+class Ninja:
+  def __init__(self, data):
+    self.id = data['id']
+    self.first_name = data['first_name']
+    self.last_name = data['last_name']
+    self.age = data['age']
+    self.created_at = data['created_at']
+    self.updated_at = data['updated_at']
+
+  @classmethod
+  def save(cls,data):
+    query = "INSERT INTO ninjas (dojo_id, first_name, last_name, age) VALUES (%(dojo_id)s, %(first_name)s, %(last_name)s, %(age)s );"
+    return connectToMySQL('dojo_ninjas').query_db(query,data) 
+  
+
+  
